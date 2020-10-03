c0727538f0a11b97b24aa0e278a5c6472b7f2b0c
Adam Matan <adam@matan.name>
2020-10-04 00:25:14 +0300

    Add tests

--- test_memoization.py
+++ test_memoization.py
@@ -1,6 +1,8 @@
 import random
 from memoize.memoize import Memoize
 
+
+# Tested functions
 @Memoize()
 def get_random():
     return random.random()
@@ -10,9 +12,19 @@ def get_random_with_args(n):
     return random.random() + n
 
 @Memoize()
+def get_another_random_with_args(n):
+    return random.random() + n
+
+@Memoize()
 def get_random_with_kwargs(n=0):
     return random.random() + n
 
+@Memoize()
+def get_random_with_args_and_kwargs(num, n=0):
+    return random.random() + num + n
+
+
+# Tests
 def test_basic_memoization():
     assert get_random() == get_random()
 
@@ -23,3 +35,12 @@ def test_args_memoization():
 def test_kwargs_memoization():
     assert get_random_with_kwargs(n=5) == get_random_with_kwargs(n=5)
     assert get_random_with_kwargs(n=5) != get_random_with_kwargs(n=6)
+
+def test_args_and_kwargs_memoization():
+    assert get_random_with_args_and_kwargs(1, n=2) == get_random_with_args_and_kwargs(1, n=2)
+    assert get_random_with_args_and_kwargs(1, n=2) != get_random_with_args_and_kwargs(1, n=3)
+    assert get_random_with_args_and_kwargs(1, n=2) != get_random_with_args_and_kwargs(2, n=2)
+
+def test_function_mixup():
+    """Make sure that different functions with the same args are cached under different keys"""
+    assert get_random_with_kwargs(n=1) != get_another_random_with_args(1)
